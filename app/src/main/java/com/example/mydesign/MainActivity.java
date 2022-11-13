89ac42b17eaa27e59e97612e80c210c05f49bc12
amit ginzberg <amitginz@gmail.com>
2022-11-13 14:15:05 +0200

    second commit

---
+++ app/src/main/java/com/example/mydesign/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mydesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView userNameEdt;
+    private TextView passwordEdt;
+    private Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userNameEdt = findViewById(R.id.email);
+        passwordEdt = findViewById(R.id.editTextTextPassword);
+        loginBtn = findViewById(R.id.button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // on below line we are getting data from our edit text.
+                String userName = userNameEdt.getText().toString();
+                String password = passwordEdt.getText().toString();
+
+                // checking if the entered text is empty or not.
+                if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, "Please enter user name and password", Toast.LENGTH_SHORT).show();
+                }
+
+                // calling a method to login our user.
+//                loginUser(userName, password);
+
+            }
+        });
+    }
+
+}
