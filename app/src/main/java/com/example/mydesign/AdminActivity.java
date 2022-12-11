24e2ac3477c34d509f0db42f397155cd81ff7b64
shlomoPearl <shlomoper94@gmail.com>
2022-12-11 15:31:58 +0200

    upload and display image

---
+++ app/src/main/java/com/example/mydesign/AdminActivity.java
@@ -0,0 +1,166 @@
+package com.example.mydesign;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.storage.UploadTask;
+
+public class AdminActivity extends AppCompatActivity {
+    private Button upload_btn;
+    private Uri imageUri;
+    private static final int IMAGE_REQUEST = 2;
+    ArrayList<String> image_list;
+    RecyclerView recyclerView;
+    StorageReference root;
+    ProgressBar progressBar;
+    ImageDisplay image_display;
+    // clothes_uploads is the brunch that all image are upload there
+    StorageReference listRef = FirebaseStorage.getInstance().getReference().child("clothes_uploads");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_activity);
+        image_list = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerview);
+        image_display = new ImageDisplay(image_list, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(null));
+        progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.VISIBLE);
+//        final ProgressDialog uploading = new ProgressDialog(this);
+//        uploading.show();
+
+        upload_btn = findViewById(R.id.SelectImage);
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+
+        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference file : listResult.getItems()) {
+                    file.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            image_list.add(uri.toString());
+                            Log.e("Item_value", uri.toString());
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            recyclerView.setAdapter(image_display);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /**
+     * to upload image from gallery to firebase account
+     */
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        launchSomeActivity.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    imageUri = data.getData();
+                    uploadImage();
+                }
+            });
+
+
+    private void uploadImage() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Uploading");
+        pd.show();
+        if (imageUri != null) {                                                                                            // the name of the image is the time + type extension
+            final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("clothes_uploads").child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            Log.d("DownloadUrl", url);
+                            pd.cancel();
+                            Toast.makeText(AdminActivity.this, "Image upload successful", Toast.LENGTH_SHORT).show();
+
+                            // to display the new image
+                            listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                                @Override
+                                public void onSuccess(ListResult listResult) {
+                                    for (StorageReference file : listResult.getItems()) {
+                                        file.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                // add only the new one
+                                                if (!image_list.contains(uri.toString())) {
+                                                    image_list.add(uri.toString());
+                                                    Log.e("Item_value", uri.toString());
+                                                }
+                                            }
+                                        }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                recyclerView.setAdapter(image_display);
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private String getFileExtension(Uri imageUri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(imageUri));
+    }
+}
---
+++ app/src/main/java/com/example/mydesign/ImageDisplay.java
@@ -0,0 +1,49 @@
+package com.example.mydesign;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+//import com.anni.uploaddataexcelsheet.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+
+public class ImageDisplay extends RecyclerView.Adapter<ImageDisplay.ViewHolder> {
+    private ArrayList<String> imageList;
+
+    public ImageDisplay(ArrayList<String> imageList, Context context) {
+        this.imageList = imageList;
+        this.context = context;
+    }
+
+    private Context context;
+    @NonNull
+    @Override
+    public ImageDisplay.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_display,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageDisplay.ViewHolder holder, int position) {
+        Glide.with(holder.itemView.getContext()).load(imageList.get(position)).into(holder.imageView);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.item);
+        }
+    }
+}
