c2f20bea7b6a016b5954856686af3f4be236e5ca
shlomoPearl <shlomoper94@gmail.com>
2022-12-12 15:32:40 +0200

    chang file name

--- app/src/main/java/com/example/mydesign/AdminStart.java
+++ app/src/main/java/com/example/mydesign/AdminStart.java
@@ -3,12 +3,20 @@ package com.example.mydesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class AdminLogin extends AppCompatActivity {
+public class AdminStart extends AppCompatActivity {
+
+    private Button sign_up; // for new user
+    private Button sign_in; // for exist user
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_login);
+        setContentView(R.layout.admin_start);
+        sign_up = findViewById(R.id.sign_up);
+        sign_in = findViewById(R.id.sign_in);
+
+
     }
 }
