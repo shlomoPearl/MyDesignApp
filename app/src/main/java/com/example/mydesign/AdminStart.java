4e98739c6dd8b46b340702da228b189a9bb13ae2
shlomoPearl <shlomoper94@gmail.com>
2022-12-18 11:50:14 +0200

    coonect buttons to signin/up

--- app/src/main/java/com/example/mydesign/AdminStart.java
+++ app/src/main/java/com/example/mydesign/AdminStart.java
@@ -2,7 +2,9 @@ package com.example.mydesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class AdminStart extends AppCompatActivity {
@@ -16,7 +18,18 @@ public class AdminStart extends AppCompatActivity {
         setContentView(R.layout.admin_start);
         sign_up = findViewById(R.id.sign_up);
         sign_in = findViewById(R.id.sign_in);
-
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminStart.this, AdminSignIn.class));
+            }
+        });
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminStart.this, AdminSignUp.class));
+            }
+        });
 
     }
 }
