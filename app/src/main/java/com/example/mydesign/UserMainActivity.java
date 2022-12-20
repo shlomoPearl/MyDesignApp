ae1ae2c0e2d3d6a383229b6ad07e047e8798cd05
amit ginzberg <amitginz@gmail.com>
2022-12-20 16:12:22 +0200

    with design disaply

--- app/src/main/java/com/example/mydesign/UserMainActivity.java
+++ app/src/main/java/com/example/mydesign/UserMainActivity.java
@@ -33,6 +33,22 @@ public class UserMainActivity extends AppCompatActivity {
                 startActivity(new Intent(UserMainActivity.this, UserClassify.class));
             }
         });
+        existing_d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(UserMainActivity.this, "jump to admin activity!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserMainActivity.this, AdminActivity.class));
+            }
+        });
+        your_own_d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(UserMainActivity.this, "jump to admin activity!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserMainActivity.this, AdminActivity.class));
+            }
+        });
 
     }
 
