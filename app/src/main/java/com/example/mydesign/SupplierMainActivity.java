2f542696bff46454ad5441421d175a86a4499d94
shlomoPearl <shlomoper94@gmail.com>
2023-01-04 12:49:40 +0200

    add Class

---
+++ app/src/main/java/com/example/mydesign/SupplierMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mydesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.supplier_activity_main);
+
+    }
+}
