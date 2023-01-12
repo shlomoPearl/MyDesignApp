c4be8a57bc5c970b6ca37253f339d51ae377bb7f
shlomoPearl <shlomoper94@gmail.com>
2023-01-12 20:34:20 +0200

    display user order from existence design

---
+++ app/src/main/java/com/example/mydesign/HolderShoppingCart.java
@@ -0,0 +1,89 @@
+package com.example.mydesign;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class HolderShoppingCart extends RecyclerView.Adapter<HolderShoppingCart.ViewHolder> {
+    private ArrayList<String> imageList;
+    private ArrayList<String[]> info;
+    public Context context;
+
+    public HolderShoppingCart(ArrayList<String> imageList, ArrayList<String[]> info, Context context) {
+        this.imageList = imageList;
+        this.info = info;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public HolderShoppingCart.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_cart_shopping,parent,false);
+        return new HolderShoppingCart.ViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull HolderShoppingCart.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.supplier_name.setText(holder.supplier_name.getText().toString() + info.get(position)[0]);
+        holder.phone.setText(holder.phone.getText().toString() + info.get(position)[1]);
+        holder.address.setText(holder.address.getText().toString() + info.get(position)[2]);
+        holder.email.setText(holder.email.getText().toString() + info.get(position)[3]);
+        holder.quantity.setText(holder.quantity.getText().toString() + info.get(position)[4]);
+        holder.size.setText(holder.size.getText().toString() + info.get(position)[5]);
+        holder.total_price.setText(holder.total_price.getText().toString() + info.get(position)[6]);
+        holder.checkBox.setChecked(info.get(position)[7].equals("true"));
+        if (info.get(position)[7].equals("true")){
+            holder.checkBox.setTextColor(Color.GREEN);
+        }
+        holder.checkBox.setEnabled(false);
+        Glide.with(holder.itemView.getContext()).load(imageList.get(position)).into(holder.imageView);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView supplier_name,phone,address,email,quantity,size,total_price;
+        CheckBox checkBox;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            supplier_name = itemView.findViewById(R.id.order_detail);
+            imageView=itemView.findViewById(R.id.image);
+            email=itemView.findViewById(R.id.email);
+            quantity=itemView.findViewById(R.id.quantity);
+            phone=itemView.findViewById(R.id.phone);
+            address=itemView.findViewById(R.id.address);
+            size=itemView.findViewById(R.id.size);
+            total_price=itemView.findViewById(R.id.total_price);
+            checkBox=itemView.findViewById(R.id.checkbox);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mydesign/UserCartShopping.java
@@ -0,0 +1,77 @@
+package com.example.mydesign;
+
+import static com.android.volley.VolleyLog.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.mydesign.menu.UserMenu;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class UserCartShopping extends UserMenu {
+    private ArrayList<String> image_list;
+    private ArrayList<String[]> info;
+    private RecyclerView recyclerView;
+    private HolderShoppingCart image_display;
+    private FirebaseFirestore store;
+    private ProgressBar progressBar;
+    private TextView count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycleview);
+        image_list = new ArrayList<>();
+        info = new ArrayList<>();
+        count = findViewById(R.id.show_text);
+        progressBar = findViewById(R.id.progress);
+        recyclerView = findViewById(R.id.recyclerview);
+        image_display = new HolderShoppingCart(image_list,info , this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(null));
+        store = FirebaseFirestore.getInstance();
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+        store.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .collection("Orders").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                            String supplier_name = document.get("Supplier Name").toString();
+                            String phone = document.get("Supplier Phone").toString();
+                            String address = document.get("Supplier Address").toString();
+                            String email = document.get("Supplier Email").toString();
+                            String quantity = document.get("Quantity").toString();
+                            String size = document.get("Size").toString();
+                            String total_price = document.get("Total Price").toString();
+                            String order_state = document.get("Order State").toString();
+                            String url = document.get("URL").toString();
+                            System.out.println("%%%%%" + url);
+                            String[] user_details = {supplier_name,phone, address ,email, quantity,
+                                                    size, total_price, order_state};
+                            image_list.add(url);
+                            info.add(user_details);
+                            count.setText("You have " + image_list.size() + " orders");
+                            recyclerView.setAdapter(image_display);
+                            progressBar.setVisibility(ProgressBar.INVISIBLE);
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+    }
+}
